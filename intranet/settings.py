14ae539a938ed1907cf192688390f6291ac1ee00
Ryan Martin <rjmartin73@gmail.com>
2025-06-20 14:34:44 -0400

    Troubleshooting 2

--- intranet/settings.py
+++ intranet/settings.py
@@ -38,7 +38,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True") == "True"
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
