0e7f013d920ab70b5be96468dc694abac103bd54
Ryan Martin <rjmartin73@gmail.com>
2025-07-01 12:37:26 -0400

    testing calendar endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import hello_api
 
 urlpatterns = [
     path('hello/', hello_api, name='hello_api'),
-    path('api/', include('api.urls')), # mount the app here
+    path('calendar/', calendar_api, name='calendar_api'),
+    path('api/', include('api.urls')),  # mount the app here
     re_path(r"^.*$", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
 from django.http import JsonResponse
 
+
 def hello_api(request):
     return JsonResponse({"message": "👋 Hello from the OEC API!"})
+
+
+def calendar_api(request):
+    return JsonResponse({"message": "📅 Calendar API endpoint!"})
