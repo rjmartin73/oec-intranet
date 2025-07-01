9269382a42536d8bd5b60e963126b4da428147e4
Ryan Martin <rjmartin73@gmail.com>
2025-07-01 12:55:48 -0400

    Forgot to import the calendar view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import hello_api
+from .views import hello_api, calendar_api
 
 urlpatterns = [
     path('hello/', hello_api, name='hello_api'),
